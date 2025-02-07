2640e8cca8adf3acf028827a10c214fdf22a7a48
admin <admin@DESKTOP-H8252V8>
2025-02-07 10:41:47 +0530

    by Snehal

---
+++ Git_project/src/git_practice/Demo.java
@@ -0,0 +1,20 @@
+package git_practice;
+
+public class Demo {
+	
+	public void m1()
+	{
+		System.out.println("this is m1 method");
+	}
+	
+	public void m2()
+	{
+		System.out.println("this is m2 method");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is main method");
+	}
+
+}
